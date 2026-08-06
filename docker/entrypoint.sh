f698515b4d172f5078aebbe118879aee955c9dba
yyyx666 <2652877047@qq.com>
2026-08-06 11:36:02 +0800

    Update entrypoint.sh

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -3,15 +3,8 @@ set -eu
 
 umask 077
 
-quality_guard_dir=/var/lib/grok2api-quality-guard
-mkdir -p "${quality_guard_dir}"
-chown grok2api:grok2api "${quality_guard_dir}"
-chmod 0700 "${quality_guard_dir}"
-
-chown grok2api:grok2api /app/config.yaml
-chmod 0600 /app/config.yaml
 sed -i "s|replace-with-at-least-32-characters|${SECRET_KEY}|g" /app/config.yaml
 sed -i "s|replace-with-base64-key|${BASE64_KEY}|g" /app/config.yaml
 sed -i "s|replace-with-a-strong-password|${API_KEY}|g" /app/config.yaml 
 
-exec su-exec grok2api:grok2api "$@"
+exec "$@"
